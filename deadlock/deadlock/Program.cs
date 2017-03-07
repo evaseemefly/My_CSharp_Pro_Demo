3d10328fbcc46dbfdea451b2e5db037ff7f52030
evaseemefly <evaseemefly@126.com>
2017-03-07 11:22:41 +0800

    3月7日 第一次创建 5510

---
+++ deadlock/deadlock/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace deadlock
+{
+    class Program
+    {
+        private static object obj_lock1 = new object();
+        private static object obj_lock2 = new object();
+        private static int count = 0;
+
+        //本示例用来展示死锁
+        static void Main(string[] args)
+        {
+            Thread thread1 = new Thread(Thread1);
+            Thread thread2 = new Thread(Thread2);
+
+            thread1.Start();
+            thread2.Start();
+
+            while (true)
+            {
+                int pre_count = count;
+                Thread.Sleep(0);
+                if (pre_count == count)
+                {
+                    Console.WriteLine("死锁！count:{0}", count);
+                }
+            }
+        }
+
+        private static void Thread1()
+        {
+            while (true)
+            {
+                //Monitor.Enter(obj_lock2);
+                //Monitor.Enter(obj_lock1);
+                Monitor.Enter(obj_lock1);
+                Monitor.Enter(obj_lock2);
+                count++;
+
+                Monitor.Exit(obj_lock1);
+                Monitor.Exit(obj_lock2);
+
+            }
+        }
+
+        private static void Thread2()
+        {
+            while (true)
+            {
+                Monitor.Enter(obj_lock1);
+                Monitor.Enter(obj_lock2);
+
+                count++;
+
+                Monitor.Exit(obj_lock1);
+                Monitor.Exit(obj_lock2);
+
+            }
+        }
+    }
+}
